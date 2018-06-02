9c0ea4ae5391ecba38a283129511d52681214950
takaneichinose <ichinose.takane@gmail.com>
2018-06-02 13:26:17 +0900

    a

--- business2/js/script.js
+++ business2/js/script.js
@@ -1,20 +1,19 @@
 $(function(){$("div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
 
-// 18.06.02 追加
-var bgMusic = document.getElementById('bg_music');
-
 // 18.06.02 音を再生する
 function initializeAudio() {
-  bgMusic.play();
-}
-
-// 18.06.02 バックグラウンドで再生される音(イベント)
-bgMusic.addEventListener('ended', function() {
-  var content = document.getElementById('content');
-  var footer  = document.getElementById('footer');
+  var bgMusic = document.getElementById('bg_music');
   
-  // フッターに固定位置にする
-  footer.classList.add('fixed');
-  // 説明：フッターは固定の縦幅がセットされていないので、Javascriptで縦幅をセットする
-  content.style.paddingBottom = document.getElementById('footer').offsetHeight + 'px';
-});
+  bgMusic.play();
+
+  // 18.06.02 バックグラウンドで再生される音(イベント)
+  bgMusic.addEventListener('ended', function() {
+    var content = document.getElementById('content');
+    var footer  = document.getElementById('footer');
+    
+    // フッターに固定位置にする
+    footer.classList.add('fixed');
+    // 説明：フッターは固定の縦幅がセットされていないので、Javascriptで縦幅をセットする
+    content.style.paddingBottom = document.getElementById('footer').offsetHeight + 'px';
+  });
+}
