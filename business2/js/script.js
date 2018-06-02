7707e43a2dd7375035be4d7e62e9fbd6e3204344
takaneichinose <ichinose.takane@gmail.com>
2018-06-02 15:36:07 +0900

    Semi-fin

--- business2/js/script.js
+++ business2/js/script.js
@@ -1,57 +1,51 @@
 $(function(){$("div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
 
 // 18.06.02 音を再生する
-function initializeAudio() {
-  // var bgMusic = document.getElementById('bg_music');
-  
-  /*
-  bgMusic.load();
+var bgMusic = document.getElementById('bg_music');
+var playBtn = document.getElementById('play_button');
+var isEnded = false;
+
+// 18.06.02 ユーザーのジェスチャーで音を再生
+function playBgMusic() {
   bgMusic.play();
-  */
-  
-  var ctx = new (window.AudioContext || window.webkitAudioContext)();
-  var src = ctx.createBufferSource();
-  var xhr = new XMLHttpRequest();
-  
-  xhr.open('GET', 'audio/sample_audio.mp3');
-  
-  xhr.responseType = 'arraybuffer';
-  
-  xhr.addEventListener('load', function (data) {
-    ctx.decodeAudioData(
-      xhr.response,
-      function(buffer) {
-        src.buffer = buffer;
-        src.loop   = false
-        
-        src.connect(ctx.destination);
-      }
-    );
-    src.start(0);
-    
-    src.onended = function() {
-      var content = document.getElementById('content');
-      var footer  = document.getElementById('footer');
-      
-      // フッターに固定位置にする
-      footer.classList.add('fixed');
-      // 説明：フッターは固定の縦幅がセットされていないので、Javascriptで縦幅をセットする
-      content.style.paddingBottom = document.getElementById('footer').offsetHeight + 'px';
-    }
-  });
-  
-  xhr.send();
-  
-  // 18.06.02 バックグラウンドで再生される音(イベント)
-  /*
-  bgMusic.addEventListener('ended', function() {
-    var content = document.getElementById('content');
-    var footer  = document.getElementById('footer');
-    
-    // フッターに固定位置にする
-    footer.classList.add('fixed');
-    // 説明：フッターは固定の縦幅がセットされていないので、Javascriptで縦幅をセットする
-    content.style.paddingBottom = document.getElementById('footer').offsetHeight + 'px';
-  });
-  */
 }
+
+// 18.06.02 フッターの間隔
+function adjustFooterSpacing() {
+  var content = document.getElementById('content');
+  var sidebar = document.getElementById('sidebar');
+  var footer  = document.getElementById('footer');
+  
+  // フッターに固定位置にする
+  footer.classList.add('fixed');
+  // 説明：フッターは固定の縦幅がセットされていないので、Javascriptで縦幅をセットする
+  if (window.outerWidth >= 644) {
+    content.style.paddingBottom = footer.offsetHeight + 'px';
+    sidebar.style.paddingBottom = '0px';
+  }
+  else {
+    content.style.paddingBottom = '0px';
+    sidebar.style.paddingBottom = footer.offsetHeight + 'px';
+  }
+}
+
+// 18.06.02 バックグラウンドで再生される音(イベント)
+bgMusic.addEventListener('ended', function() {
+  adjustFooterSpacing();
+  
+  bgMusic.parentNode.removeChild(bgMusic);
+  playBtn.parentNode.removeChild(playBtn);
+  
+  bgMusic = null;
+  isEnded = true;
+});
+
+playBtn.addEventListener('click', function() {
+  playBgMusic();
+});
+
+window.addEventListener('resize', function() {
+  if (isEnded) {
+    adjustFooterSpacing();
+  }
+});
