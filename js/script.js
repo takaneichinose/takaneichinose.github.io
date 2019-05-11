9365f062bed4e22ae0a613e0f35ecf74bc685fff
Takane Ichinose <ichinose.takane@gmail.com>
2019-05-11 14:04:32 +0900

    Redesign of the page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var statBars = document.querySelectorAll('.stat-bar');
 
 function setStatThread(i) {
-	setTimeout(function() {
-		statBars[i].classList.add('stat-' + statBars[i].getAttribute('data-stat'));
-		if (i < statBars.length) {
-			setStatThread(i + 1);
-		}
-	}, 100);
+  setTimeout(function () {
+    statBars[i].classList.add('stat-' + statBars[i].getAttribute('data-stat'));
+    if (i < statBars.length) {
+      setStatThread(i + 1);
+    }
+  }, 200);
 }
 
-setTimeout(function() {
-	setStatThread(0);
+setTimeout(function () {
+  setStatThread(0);
 }, 500);
 
